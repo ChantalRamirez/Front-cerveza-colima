2eb61a764a1ea12bd8008cf0d943a6879063223d
Omar Ortega <omarvalentin@gmail.com>
2020-09-30 02:22:02 -0500

    Se agregó Edición de users

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,7 @@ import Layout from "../components/Layout";
 //users
 import UsersList from "../pages/users/UsersList";
 import UserAdd from "../pages/users/UserAdd";
+import UserEdit from "../pages/users/UserEdit";
 
 function App() {
   return (
@@ -26,13 +27,12 @@ function App() {
             <Route exact path="/" component={Start} /> 
             <Route exact path="/home" component={Home} />
             <Route exact path="/products" component={ProductCatalog} />
-            {/* <Route exact path="/products" component={Products} /> */}
-            {/* <Route exact path="/products/new" component={ProductNew} /> */}
             <Route exact path="/products/add" component={ProductAdd} />
             <Route path="/products/edit/:id" component={ProductEdit} />
             <Route exact path="/products/list" component={ProductList} />
             <Route exact path="/users/list" component={UsersList} />
             <Route exact path="/users/add" component={UserAdd} />
+            <Route exact path="/users/edit/:id" component={UserEdit} />
             <Route exact path="/404" component={MiComponente404} />
             <Redirect from="*" to="/404" />
           </Switch>
