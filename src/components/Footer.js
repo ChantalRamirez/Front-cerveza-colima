77abdf7d103276ed98c2c86b6b52a3193757a4c3
Chantal Ramirez <ramirez_yessica@ucol.mx>
2020-10-12 20:46:22 -0500

    Iconos redes sociales

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import './styles/Footer.css';
+import { Link } from 'react-router-dom';
+import iconFacebook from '../images/facebook.svg';
+import iconTwitter from '../images/twitter.svg';
+import iconInstagram from '../images/instagram.svg';
 
 class Footer extends React.Component {
   render() {
     return (
       <div className="Footer">
-       Derechos reservados <br/>
-       Contacto Cerveza Colima
+        <div className="Footer__container">
+          Derechos reservados <br/>
+          Contacto Cerveza Colima
+        </div> 
+        <div>
+          <Link to="https://www.facebook.com/">
+            <img
+              className="social-link"
+              src={iconFacebook}
+              alt="facebook icon"
+            />
+          </Link>
+          <Link to="https://twitter.com/">
+            <img
+              className="social-link"
+              src={iconTwitter}
+              alt="twitter icon"
+            />
+          </Link>
+          <Link to="https://instagram.com/">
+            <img
+              className="social-link"
+              src={iconInstagram}
+              alt="instagram icon"
+            />
+          </Link>
+        </div>
       </div>
-    );
+    )
   }
 }
 
