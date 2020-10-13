89b0292c3097be3a1a6e45e8ec0d3cbed19f7bc7
Omar Ortega <omarvalentin@gmail.com>
2020-10-13 08:54:58 -0500

    Cambio de layout

--- src/pages/ProductCatalog.js
+++ src/pages/ProductCatalog.js
@@ -1,45 +1,40 @@
 import React, { useEffect, useState } from "react";
 import Product from "./Product";
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 import axios from "axios";
-import './styles/Products.css';
-
+import "./styles/Products.css";
 
 const CatalogoProductos = () => {
-  const [ products, setProducts]  = useState([])
-  
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-      const obtenerProductos = async () => {
-        const url = "https://cerveceria-app.herokuapp.com/getProducts";
-        const resultado = await axios.get(url);
-        setProducts(resultado.data.products);
-      };
-      obtenerProductos();
- 
+    const obtenerProductos = async () => {
+      const url = "https://cerveceria-app.herokuapp.com/getProducts";
+      const resultado = await axios.get(url);
+      setProducts(resultado.data.products);
+    };
+    obtenerProductos();
   }, []);
-  
-    console.log(products);
+
+  console.log(products);
 
   return (
     <Layout>
-      
       <div className="Container">
-      <div className="Products__hero">
-                <div className="Products__container">
-                  <h3>Catálogo de productos</h3>
-                </div> 
-              </div>
-        
-              <div className="Products__container">
-        <div className="row">
-          {products.map((product) => (
-            <Product key={product.id} product={product} />
-          ))}
+        <div className="Products__hero">
+          <div className="Products__container">
+            <h3>Catálogo de productos</h3>
+          </div>
         </div>
+
+        <div className="Products__container">
+          <div className="row">
+            {products.map((product) => (
+              <Product key={product.id} product={product} />
+            ))}
+          </div>
         </div>
       </div>
-      
     </Layout>
   );
 };
